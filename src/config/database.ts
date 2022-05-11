824e2dd3f867b7e7cad68be901349975e5c1482b
Romesh Kosme <romesh@startupflux.com>
2022-05-11 10:57:42 +0530

    Inital Commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,21 @@
-import { MongoClient } from "mongodb";
+import { Client } from "pg";
 
 async function connect() {
-  MongoClient.connect(process.env.MONGO_URL)
-    .then((client: any) => {
-      console.log("Connected to MongoDB");
+  const client = new Client({
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: "testdb",
+  });
+  client
+    .connect()
+    .then(() => {
+      console.log("Connected to postgres!");
     })
     .catch((err) => {
-      console.log("Error connecting to MongoDB: ", err);
+      console.log(err);
     });
 }
+
 export default connect;
